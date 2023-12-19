9da0fcf1027bba455246d26a7c0174c9cca646fa
GanaelD <104094089+GanaelD@users.noreply.github.com>
2023-12-19 10:05:38 +0100

    Update Summon.cs: Take items into account

--- Summon.cs
+++ Summon.cs
@@ -1,12 +1,33 @@
 static void Summon(List<Card> hand, List<Card> myBoard, List<Card> hisBoard, int mana)
     {
+        int manaTemp = mana;
         Console.Error.WriteLine("summon");
         for(int i=0; i<hand.Count;i++)
         {
-            if(hand[i].cost<mana)
+            if(hand[i].cost<manaTemp)
             {
-                Player.SummonCard(hand[i]);
-                mana=0;
+                switch (hand[i].cardType) {
+                    case 0:
+                        Player.SummonCard(hand[i]);
+                        manaTemp -= hand[i].cost;
+                        break;
+                    case 1:
+                        if (myBoard.Count > 0) {
+                            Player.UseItem(hand[i], myBoard[0]);
+                            manaTemp -= hand[i].cost;
+                        }
+                        break;
+                    case 2:
+                        if (hisBoard.Count > 0) {
+                            Player.UseItem(hand[i], hisBoard[0]);
+                            manaTemp -= hand[i].cost;
+                        }
+                        break;
+                    case 3:
+                        Player.UseItem(hand[i], null);
+                        manaTemp -= hand[i].cost;
+                        break;
+                }
             }
         }
     }
