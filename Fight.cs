94a0dc186da48b5349538dfc9194a5fc4ddbdd6d
bapteman <baptiste.seneclauze@gmail.com>
2023-12-20 10:58:26 +0100

    commit meilleure gestion taunts

--- Fight.cs
+++ Fight.cs
@@ -1,6 +1,27 @@
 static void Fight()
     {
         Console.Error.WriteLine("fight");
+        List<Card> provocs = hisBoard.Where(card => card.abilities.Contains('G')).ToList();
+        foreach (Card guard in provocs)
+        {
+            List<Card> killers = attackers.Where(card => card.attack >= guard.defense
+                || card.abilities.Contains('L')).ToList();
+            Card attacker;
+            if (killers.Count > 0)
+            {
+                attacker = killers.Aggregate(killers[0], (acc, cur) => cur.attack < acc.attack ? cur : acc);
+            } else {
+                attacker = myBoard[0];
+            }
+            foreach (Card myCard in attackers)
+            {
+                if (myCard.attack > guard.defense && myCard.attack < attacker.attack) 
+                {
+                    attacker = myCard;
+                }
+            }
+        }
+
         foreach (Card myCard in myBoard)
         {
             Card guard = hisBoard.FirstOrDefault(card => card.abilities.Contains("G") && card.defense > 0);
@@ -9,8 +30,18 @@ static void Fight()
                 Player.AttackCard(myCard, guard);
                 if (guard != null)
                 {
-                    guard.defense -= myCard.attack;
+                    if (guard.abilities.Contains("W"))
+                    {
+                        // If the target has Ward, it takes no damage but loses Ward
+                        guard.abilities.Replace('W', '-');
+
+                    } else {
+                        guard.defense = myCard.abilities.Contains('L')
+                            ? 0
+                            : guard.defense - myCard.attack;
+                    }
                 }
+
             }  
         }
     }
