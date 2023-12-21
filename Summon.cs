5754be776f56ad00c293e0f210e9aeed0fceec09
GanaelD <104094089+GanaelD@users.noreply.github.com>
2023-12-21 09:43:21 +0100

    Update Summon.cs: Use a while loop instead of foreach to be able to choose the next card to play

--- Summon.cs
+++ Summon.cs
@@ -1,42 +1,43 @@
 static void Summon(int mana)
     {
-        int manaTemp = mana;
         Console.Error.WriteLine("summon");
-        foreach (Card card in hand)
+        int manaRemaining = mana;
+        while (canBePlayed.Count > 0)
         {
-            if (card.cost <= manaTemp)
-            {
-                switch (card.cardType) {
-                    case 0:
-                        // Case 0 represents creatures
-                        Player.SummonCard(card);
-                        myBoard.Add(card);
-                        if(card.abilities.Contains("C"))
-                        {
-                            attackers.Add(card);
-                        }
-                        manaTemp -= card.cost;
-                        break;
-                    case 1:
-                        // Case 1 represents green items
-                        if (myBoard.Count > 0) {
-                            Player.UseItem(card, myBoard[0]);
-                            manaTemp -= card.cost;
-                        }
-                        break;
-                    case 2:
-                        // Case 2 represents red items
-                        if (hisBoard.Count > 0) {
-                            Player.UseItem(card, hisBoard[0]);
-                            manaTemp -= card.cost;
-                        }
-                        break;
-                    case 3:
-                        // Case 3 represents blue items
-                        Player.UseItem(card, null);
-                        manaTemp -= card.cost;
-                        break;
-                }
+            Card card = canBePlayed[0];
+            switch (card.cardType) {
+                case 0:
+                    // Case 0 represents creatures
+                    Player.SummonCard(card);
+                    myBoard.Add(card);
+                    if (card.abilities.Contains('C'))
+                    {
+                        attackers.Add(card);  
+                    }
+                    Console.Error.WriteLine($"Abilities: {card.abilities}");
+                    manaRemaining -= card.cost;
+                    break;
+                case 1:
+                    // Case 1 represents green items
+                    if (myBoard.Count > 0) {
+                        Player.UseItem(card, myBoard[0]);
+                        manaRemaining -= card.cost;   
+                    }
+                    break;
+                case 2:
+                    // Case 2 represents red items
+                    if (hisBoard.Count > 0) {
+                        Player.UseItem(card, hisBoard[0]);
+                        manaRemaining -= card.cost;
+                    }
+                    break;
+                case 3:
+                    // Case 3 represents blue items
+                    Player.UseItem(card, null);
+                    manaRemaining -= card.cost;
+                    break;
             }
+            canBePlayed.Remove(card);
+            canBePlayed.RemoveAll(card => card.cost > manaRemaining);
         }
     }
