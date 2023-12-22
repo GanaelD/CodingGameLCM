b3455c931ae5c97a434554ce9506ffcb09d8e2ba
bapteman <baptiste.seneclauze@gmail.com>
2023-12-22 12:41:40 +0100

    commit gestion timeout + scoring basique

--- Fight.cs
+++ Fight.cs
@@ -1,6 +1,24 @@
-static void ScoreAttack(Card attacker, Card Attacked)
+static int ScoreAttack(Card attacker, Card attacked)  //rajouter score si la créature adverse a bcp d'attaque
     {
-        return 10;
+        int score = 0;
+        if(attacker.attack > attacked.defense && !attacked.abilities.Contains('W'))
+        {
+            score += 10 + Math.Abs(attacked.attack - attacker.defense);
+            if(attacker.abilities.Contains('B'))
+            {
+                score += attacker.attack-attacked.defense;
+            }else {
+                score += 5-(attacker.attack-attacked.defense);
+            }
+
+        }else if(attacker.abilities.Contains('L') && !attacked.abilities.Contains('W'))
+        {
+            score += 10 + Math.Abs(attacked.attack - attacker.defense)*2;
+        }else if(attacker.attack < attacked.defense || attacked.abilities.Contains('W'))
+        {
+            score += attacker.defense - attacked.attack;
+        }
+        return score;
     }
 
 static void Fight()
@@ -8,10 +26,12 @@ static void Fight()
         Console.Error.WriteLine("fight");
         List<Card> provocs = hisBoard.Where(card => card.abilities.Contains('G')).ToList();
         int maxScore = 0;
-        Card bestAttacker = new Card();
-        Card defender = new Card();
+        Card bestAttacker = attackers[0];
+        Card defender;
         while(provocs.Count > 0 && attackers.Count > 0)
         {
+            defender = provocs[0];
+            maxScore =0;
             foreach (Card guard in provocs)
             {
                 foreach (Card attacker in attackers){
@@ -24,10 +44,16 @@ static void Fight()
                 }
             }
             AttackCard(bestAttacker,defender);
+            Console.Error.WriteLine(attackers.Any(attack => attack.instanceId==bestAttacker.instanceId));
+            Console.Error.WriteLine(attackers.Count);
+            foreach (Card attacker in attackers){
+                Console.Error.WriteLine(attacker.instanceId);
+            }
+            
             provocs = hisBoard.Where(card => card.abilities.Contains('G')&&card.defense>0).ToList();
         }
 
-        if(attackers.Sum(attacker => attacker.attack) >= pdv du mec)     //changer pour mettre les points de vie de l'adversaire
+        if(attackers.Sum(attacker => attacker.attack) >= 30)     //changer pour mettre les points de vie de l'adversaire
         {
             foreach(Card attacker in attackers)
             {
@@ -35,11 +61,14 @@ static void Fight()
             }
         }
 
-        bestAttacker = new Card();
-        defender = new Card();
+        bestAttacker = null;
+        defender = null;
         maxScore = 0;
         while (attackers.Count > 0 && hisBoard.Count > 0)
         {
+            bestAttacker = attackers[0];
+            defender = hisBoard[0];
+            maxScore = 0;
             foreach (Card attacker in attackers)
             {
                 foreach (Card defend in hisBoard){
@@ -51,7 +80,7 @@ static void Fight()
                     }
                 }
             }
-            if(maxScore>scorePalier)     //score palier à determiner
+            if(maxScore>15)     //score palier à determiner
             {
                 AttackCard(bestAttacker,defender);
             }else
@@ -60,9 +89,9 @@ static void Fight()
             }
             
         }
-        if(attacker.Count > 0)
+        if(attackers.Count > 0)
         {
-            foreach(Card attacker in attackers)
+            foreach(Card attacker in attackers.ToList())
             {
                 AttackCard(attacker, null);
             }
